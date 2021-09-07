62e1949492d0f73f208b56db1918cdfd29e52036
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-07 08:53:20 -0500

    Complete footer for desktop screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Navbar, Image } from 'react-bootstrap';
-// import { Link } from 'react-bootstrap/lib/Navbar';
+import { Navbar } from 'react-bootstrap';
 
 export default function Footer() {
 	return (
@@ -9,40 +8,42 @@ export default function Footer() {
 				<div className='row'>
 					<div className='col-lg-5 col-xs-12 about-company mt-lg-3'>
 						<Navbar.Brand className='navbar-brand'>
-							{/* 							<Image src='' width='30px' height='30px' />
-                     <Link>
-
-                     </Link> */}
-							<a className='footer-navbar-brand'>Andre Romanovich</a>
+							<a href='#homePage' className='footer-navbar-brand'>
+								Andre Romanovich
+							</a>
 						</Navbar.Brand>
-						<p className='pr-5'>
-							We are a brand whose incredible family of associates put
-							their souls in hospitality everyday. The needs of our
-							guests and affiliates are at the forefront of what we do.
-							Through authenticity and innovation, great memorable
-							experiences begin with us!
+						<p className='pr-5 mission-statement'>
+							"I put my heart and soul into my work. The needs of my
+							clients are at the forefront of what I do everyday. Through
+							programming, engineering, art, research, and innovation, I
+							develop software and websites that are artistic,
+							functional, and user friendly; which evolve with my client
+							business needs."
 						</p>
 						<p className='social-icons'>
 							<a
 								href='https://www.facebook.com'
 								target='_blank'
 								className='social-icon facebook'
+								rel='noreferrer'
 							>
-								<i className='fa fa-facebook-square'></i>
+								<i class='fab fa-facebook-square'></i>
 							</a>
 							<a
 								href='https://twitter.com/'
 								target='_blank'
 								className='social-icon twitter'
+								rel='noreferrer'
 							>
-								<i className='fa fa-twitter-square'></i>
+								<i class='fab fa-twitter-square'></i>
 							</a>
 							<a
-								href='https://www.instagram.com/'
+								href='https://github.com/'
 								target='_blank'
-								className='social-icon instagram'
+								className='social-icon github'
+								rel='noreferrer'
 							>
-								<i className='fa fa-instagram'></i>
+								<i class='fab fa-github-square'></i>
 							</a>
 						</p>
 					</div>
@@ -51,34 +52,19 @@ export default function Footer() {
 						<div className='link-container'>
 							<ul className='m-0 p-0 link-list'>
 								<li className='link'>
-									{/* <Link href='/'>
-										<a>Home</a>
-									</Link> */}
-									<a href='#'>Home</a>
+									<a href='#homePage'>Home</a>
 								</li>
 								<li className='link'>
-									{/* 									<Link href='/about'>
-										<a>About</a>
-									</Link> */}
-									<a href='#'>Home</a>
+									<a href='#aboutPage'>About</a>
 								</li>
 								<li className='link'>
-									{/* 									<Link href='/search'>
-										<a>Rooms</a>
-									</Link> */}
-									<a href='#'>Home</a>
+									<a href='#skillsPage'>Skills</a>
 								</li>
 								<li className='link'>
-									{/* 									<Link href='/testimonials'>
-										<a>Testimonials</a>
-									</Link> */}
-									<a href='#'>Home</a>
+									<a href='#portfolioPage'>Portfolio</a>
 								</li>
 								<li className='link'>
-									{/* 									<Link href='/contact'>
-										<a>Contact</a>
-									</Link> */}
-									<a href='#'>Home</a>
+									<a href='#contactPage'>Contact</a>
 								</li>
 							</ul>
 						</div>
@@ -95,21 +81,22 @@ export default function Footer() {
 							Work-hours: 9:00am - 12:00am
 						</p>
 						<p className='about-location-item mb-1 link'>
-							<i className='fa fa-facebook-square'></i>
+							<i class='fas fa-phone-alt'></i>&nbsp;
 							<a href='tel:901-425-5525'>123-456-7890</a>
 						</p>
 						<p className='about-location-item mb-2 link'>
-							<i className='fa fa-facebook-square'></i>
+							<i class='fas fa-envelope-open-text'></i>&nbsp;
 							<a
 								href='mailto:mdbdrrhm2@gmail.com'
 								target='_blank'
 								rel='noreferrer'
 							>
-								rooms@e-reserve.com
+								andre_romanovich@gmail.com
 							</a>
 						</p>
 					</div>
 				</div>
+				<hr className='footer-horizontal-line' />
 				<div className='row'>
 					<div className='col copyright'>
 						<p className='copyright-item text-center link'>
@@ -124,7 +111,8 @@ export default function Footer() {
 								<br />
 							</small>
 							<small className='copyright-text'>
-								&copy;&nbsp;2013 - 2021 Andre Romanovich, All rights reserved
+								&copy;&nbsp;2013 - 2021 Andre Romanovich, All rights
+								reserved
 							</small>
 						</p>
 					</div>
