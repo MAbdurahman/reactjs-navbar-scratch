74494a32a71e9eadb0678ca018739ceea12737c4
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-08 07:57:05 -0500

    Modify: #header-heading and #header-heading--sub

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,32 @@ export default function HomePage() {
 	return (
 		<div id='homePage' className='header pages scrollspy'>
 			<div className='header_overlay'></div>
-			<h1>homePage</h1>
+			<div id='header-content'>
+				<div id='header-content--inner' className='text-center'>
+					<div id='header-heading'>
+						<h1 id='header-heading--one'>Andre</h1>
+						<h1 id='header-heading--two'>Romanovich</h1>
+					</div>
+					<div id='header-heading--sub'>
+						<h3>Software and Web Developer</h3>
+					</div>
+					<div id='header-heading--btn'>
+						<a
+							href='#about'
+							id='heading--btn'
+							class='btn-general btn-home'
+							role='button'
+						>
+							View More
+						</a>
+					</div>
+					<div id='arrow-content'>
+						<a href='#about' id='arrow-down'>
+							<i className='fas fa-chevron-down'></i>
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
